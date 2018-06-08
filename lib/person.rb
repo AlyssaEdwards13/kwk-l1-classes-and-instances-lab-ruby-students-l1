346902d928cef01d8bd5cdddf268c362d2a451df
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-08 17:08:47 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,29 @@
-# person.rb
+class Person
+  
+  def name=(person_name)
+    @This_person_name = person_name
+  end 
+
+  def name
+    @This_person_name
+  end 
+end 
+
+class Person_2
+  
+  def name_2=(person_name_2)
+  @This_persons_name = person_name_2
+end 
+
+  def name_2
+    @This_persons_name
+  end 
+end 
+
+adele_goldberg = Person.new 
+adele_goldberg.name = "Adele Goldberg"
+puts adele_goldberg.name 
+
+alan_kay = Person_2.new 
+alan_kay.name_2 = "Alan Kay"
+puts alan_kay.name_2
